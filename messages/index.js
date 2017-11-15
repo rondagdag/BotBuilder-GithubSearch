19abfd687a11a362dfc1d756cb406767139f08f3
Ron Dagdag <rlyle78@gmail.com>
2017-11-14 18:21:05 -0600

    initial

---
+++ messages/index.js
@@ -0,0 +1,87 @@
+/*-----------------------------------------------------------------------------
+This template demonstrates how to use Waterfalls to collect input from a user using a sequence of steps.
+For a complete walkthrough of creating this type of bot see the article at
+https://aka.ms/abs-node-waterfall
+-----------------------------------------------------------------------------*/
+"use strict";
+var builder = require("botbuilder");
+var botbuilder_azure = require("botbuilder-azure");
+var path = require('path');
+var githubClient = require('./github-client.js');
+
+
+var useEmulator = (process.env.NODE_ENV == 'development');
+
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword'],
+    stateEndpoint: process.env['BotStateEndpoint'],
+    openIdMetadata: process.env['BotOpenIdMetadata']
+});
+
+var dialog = new builder.IntentDialog();
+dialog.matches(/^search/i, [
+    function(session, args, next) {
+        if (session.message.text.toLowerCase() == 'search') {
+            builder.Prompts.text(session, 'Who are you looking for?');
+        } else {
+            var query = session.message.text.substring(7);
+            next({ response: query });
+        }
+    },
+    function(session, result, next) {
+        var query = result.response;
+        if (!query) {
+            session.endDialog('Request cancelled');
+        } else {
+            githubClient.executeSearch(query, function(profiles) {
+                var totalCount = profiles.total_count;
+                if (totalCount == 0) {
+                    session.endDialog('Sorry, no results found.');
+                } else if (totalCount > 10) {
+                    session.endDialog('More than 10 results were found. Please provide a more restrictive query.');
+                } else {
+                    session.dialogData.property = null;
+                    var usernames = profiles.items.map(function(item) { return item.login });
+                    builder.Prompts.choice(session, 'What user do you want to load?', usernames);
+                }
+            });
+        }
+    },
+    function(session, result, next) {
+        var username = result.response.entity;
+        githubClient.loadProfile(username, function(profile) {
+            var card = new builder.ThumbnailCard(session);
+
+            card.title(profile.login);
+
+            card.images([builder.CardImage.create(session, profile.avatar_url)]);
+
+            if (profile.name) card.subtitle(profile.name);
+
+            var text = '';
+            if (profile.company) text += profile.company + ' \n';
+            if (profile.email) text += profile.email + ' \n';
+            if (profile.bio) text += profile.bio;
+            card.text(text);
+
+            card.tap(new builder.CardAction.openUrl(session, profile.html_url));
+
+            var message = new builder.Message(session).attachments([card]);
+            session.send(message);
+        });
+    }
+]);
+
+bot.dialog('/', dialog);
+
+if (useEmulator) {
+    var restify = require('restify');
+    var server = restify.createServer();
+    server.listen(3978, function() {
+        console.log('test bot endpont at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());
+} else {
+    module.exports = { default: connector.listen() }
+}
